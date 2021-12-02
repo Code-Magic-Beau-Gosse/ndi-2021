e1faac2bf521182abb64473b6c798acb921c1b92
Emile Rolley <emile.rolley@tuta.io>
2021-12-03 00:43:20 +0100

    feat(front): Data.toElem

--- frontend/src/Data.elm
+++ frontend/src/Data.elm
@@ -1,4 +1,9 @@
-module Data exposing (Data, toString)
+module Data exposing (Data(..), toElem, toString)
+
+import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
 
 
 type Data
@@ -24,24 +29,47 @@ toString data =
             "LifeGuard {\n"
                 ++ "id: "
                 ++ String.fromInt infos.id
-                ++ "\n,firstName: "
+                ++ "\n, firstName: "
                 ++ infos.firstName
-                ++ "\n,lastName: "
+                ++ "\n, lastName: "
                 ++ infos.lastName
-                ++ "\n,age: "
+                ++ "\n, age: "
                 ++ String.fromInt infos.age
-                ++ "\n,bio: "
+                ++ "\n, bio: "
                 ++ String.fromInt infos.age
                 ++ "\n}"
 
         Boat infos ->
-            "LifeGuard {\n"
+            "Boat {\n"
                 ++ "id: "
                 ++ String.fromInt infos.id
-                ++ "\n,name: "
+                ++ "\n, name: "
                 ++ infos.name
-                ++ "\n,matricule: "
+                ++ "\n, matricule: "
                 ++ infos.matricule
-                ++ "\n,picture: "
+                ++ "\n, picture: "
                 ++ infos.picture
                 ++ "\n}"
+
+
+toElem : Data -> Element msg
+toElem data =
+    case data of
+        LifeGuard infos ->
+            el []
+                (column
+                    []
+                    [ row []
+                        [ text infos.firstName
+                        , text infos.lastName
+                        ]
+                    , text (String.fromInt infos.age)
+                    ]
+                )
+
+        Boat infos ->
+            el []
+                (column
+                    [ width fill ]
+                    [ text infos.name ]
+                )
--- frontend/src/Pages/Home_.elm
+++ frontend/src/Pages/Home_.elm
@@ -1,11 +1,12 @@
 module Pages.Home_ exposing (Model, Msg, page)
 
+import Data exposing (Data(..), toElem, toString)
 import Debug exposing (toString)
 import Dict exposing (Dict)
+import Element exposing (..)
+import Element.Font as Font
+import Element.Input as Input
 import Gen.Params.Home_ exposing (Params)
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
 import Page exposing (Page)
 import Request exposing (Request)
 import Result exposing (toMaybe)
@@ -27,17 +28,15 @@ page shared req =
 
 
 type alias Model =
-    { url : String
+    { request : String
     , result : String
-    , urls : Dict String String
     }
 
 
 init : Model
 init =
-    { url = ""
+    { request = ""
     , result = ""
-    , urls = Dict.empty
     }
 
 
@@ -52,27 +51,65 @@ type Msg
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        SearchReq url ->
-            { model | url = url }
+        SearchReq request ->
+            { model | request = request }
 
 
 
 -- VIEW
 
 
-viewInput : String -> String -> String -> (String -> msg) -> Html msg
-viewInput t p v toMsg =
-    input [ type_ t, placeholder p, value v, onInput toMsg, class "url-input" ] []
+dataTest : List Data
+dataTest =
+    [ LifeGuard
+        { id = 0
+        , firstName = "Emile"
+        , lastName = "Rolley"
+        , age = 21
+        , bio = "Doing NDI..."
+        }
+    , LifeGuard
+        { id = 1
+        , firstName = "Gilles"
+        , lastName = "Gilles"
+        , age = 20
+        , bio = "Doing NDI... too"
+        }
+    , Boat
+        { id = 2
+        , name = "L'Hermione"
+        , matricule = "0HASDF-ADF"
+        , picture = "https://s1.qwant.com/thumbr/0x380/0/4/ebf6e0d454b4ba0ef53f84ca73f553128230f4964356e152058d2cf8ab184e/hermione_en_mer_op_9149_0.jpg?u=https%3A%2F%2Fminiweb.metropoletpm.fr%2Fsites%2Fnew.tpm-agglo.fr%2Ffiles%2Fhermione_en_mer_op_9149_0.jpg&q=0&b=1&p=0&a=0"
+        }
+    ]
 
 
 view : Model -> View Msg
 view model =
     { title = "Homepage"
-    , body =
-        [ Html.div [ class "home-page" ]
-            [ Html.div
-                [ class "input-container" ]
-                [ viewInput "text" "Search ..." model.url SearchReq ]
+    , element =
+        column [ centerX, height fill ]
+            [ el
+                [ Font.size 100
+                , Font.family
+                    [ Font.typeface "Helvetica", Font.sansSerif ]
+                ]
+                (text "D1kerque Gang")
+            , el [ centerX, centerY ]
+                (Input.search
+                    []
+                    { onChange = SearchReq
+                    , text = model.request
+                    , placeholder = Just (Input.placeholder [] (text "Georges..."))
+                    , label = Input.labelLeft [] (text "")
+                    }
+                )
+            , column
+                [ centerX, centerY ]
+                (List.foldl
+                    (\e acc -> Data.toElem e :: acc)
+                    []
+                    dataTest
+                )
             ]
-        ]
     }
