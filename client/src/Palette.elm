93760c8b7da1dbde3414b1a52716c867d0be290b
GIlles Souton <gilles.souton@etu.u-bordeaux.fr>
2021-12-03 06:36:13 +0100

    Add Palette.elmmm.....

---
+++ client/src/Palette.elm
@@ -0,0 +1,33 @@
+module Palette exposing (..)
+
+import Element exposing (Color, rgb, rgb255)
+
+
+black : Color
+black =
+    rgb255 0 0 0
+
+
+imperialRed : Color
+imperialRed =
+    rgb255 230 57 70
+
+
+honeydew : Color
+honeydew =
+    rgb255 241 250 238
+
+
+powderBlue : Color
+powderBlue =
+    rgb255 168 218 220
+
+
+celadonBlue : Color
+celadonBlue =
+    rgb255 69 123 157
+
+
+prussianBlue : Color
+prussianBlue =
+    rgb255 29 53 87
