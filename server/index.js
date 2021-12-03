7f25e23a25dc2d97bb060ebfccc4268738d072ab
GIlles Souton <gilles.souton@etu.u-bordeaux.fr>
2021-12-03 01:52:44 +0100

    add get method search/all

--- server/index.js
+++ server/index.js
@@ -1,17 +1,42 @@
 const app = require("express")();
 const PORT = 8080;
-const lifeGuards = "data/lifeguards";
-const boats = "data/boats";
 const fs = require("fs");
 const path = require("path");
 
+const lifeguardPath = __dirname + "/data/lifeguards";
+const boatPath = __dirname + "/data/boats";
+
 app.listen(PORT, () => console.log(`It's running on localhost:${PORT}`));
 
-app.get("/search", (req, res) => {
-  res.status(200).send({
-    name: "name",
-    age: 20,
+function scanDirectory(directory) {
+  let files = fs.readdirSync(path.resolve(directory));
+  let obj = {};
+  files.forEach(function (file) {
+    obj[file.replace(/\.[^/.]+$/, "")] = file;
   });
+  return obj;
+}
+
+let lifeguardFiles = scanDirectory("data/lifeguards");
+let boatFiles = scanDirectory("data/boats");
+
+console.log(lifeguardFiles);
+console.log(boatFiles);
+
+function getObjects(jsonPath, Jsonfiles) {
+  let result = [];
+  for (const file in Jsonfiles) {
+    console.log(Jsonfiles[file]);
+    let rawdata = fs.readFileSync(path.resolve(jsonPath, Jsonfiles[file]));
+    result.push(JSON.parse(rawdata));
+  }
+  return result;
+}
+
+app.get("/search/all", (req, res) => {
+  let lifeguards = getObjects(lifeguardPath, lifeguardFiles);
+  //let boats = getObjects(boatPath, boatFiles);
+  res.status(200).send({ lifeguards });
 });
 
 app.get("/search/name/:name", (req, res) => {
@@ -27,6 +52,7 @@ app.post("/search/:id", (req, res) => {
   const { logo } = req.body;
 });
 
-let rawdata = fs.readFileSync(path.resolve(lifeGuards, "lifeguard.json"));
-let lifeguard = JSON.parse(rawdata);
-console.log(lifeguard["1"]);
+//let rawdata = fs.readFileSync(path.resolve(lifeGuards, "lifeguard.json"));
+//let lifeguard = JSON.parse(rawdata);
+//console.log(lifeguard["1"]);
+//console.log();
